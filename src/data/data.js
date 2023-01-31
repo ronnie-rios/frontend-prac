714678ed2247c23915d5e9fafc137521e1e05ae7
Ronnie Rios <ronniesmail14@gmail.com>
2023-01-31 09:47:56 -0600

    frontend prac initial setup

---
+++ src/data/data.js
@@ -0,0 +1,17 @@
+export const data = [
+    {
+        id: 1,
+        name: 'momo',
+        power: 'running around'
+    },
+    {
+        id: 2,
+        name: 'jaxon',
+        power: 'lounging'
+    },
+    {
+        id: 3,
+        name: 'bizkit',
+        power: 'licking windows'
+    },
+]
